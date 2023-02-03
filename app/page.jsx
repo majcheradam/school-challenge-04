524cd0f422782490fc633e1d59c30ea449c3ef95
Domun <zwolinskid1@gmail.com>
2023-02-03 18:45:04 +0100

    Update 5
    - fixed bugs
    - added new images
    - added new code

--- app/page.jsx
+++ app/page.jsx
@@ -78,31 +78,56 @@ export default function Home() {
 
       <main>
         <section className="first-content">
-          <div className="first-content-item">
+          <div className="content-item">
             <h2>RECENT LAUNCH</h2>
             <h1>STARLINK MISSION</h1>
-            <button className="first-content-button">REWATCH</button>
+            <button className="content-button">REWATCH</button>
           </div>
         </section>
         <section className="second-content">
-          <Image
-            src="/back_image_b.jpg"
-            alt=""
-            width="7680"
-            height="4320"
-            className="second"
-          />
+          <div className="content-size">
+            <div className="content-item">
+              <h2>RECENT LAUNCH</h2>
+              <h1>STARLINK MISSION</h1>
+              <button className="content-button">REWATCH</button>
+            </div>
+          </div>
         </section>
         <section className="third-content">
-          <Image
-            src="/back_image_c.jpg"
-            alt=""
-            width="7680"
-            height="4320"
-            className="third"
-          />
+          <div className="content-size">
+            <div className="content-item">
+              <h2>RECENT LAUNCH</h2>
+              <h1>STARLINK MISSION</h1>
+              <button className="content-button">REWATCH</button>
+            </div>
+          </div>
+        </section>
+        <section className="fourth-content">
+          <div className="content-size">
+            <div className="content-item">
+              <h1>
+                STARSHIP SELECTED
+                <br /> BY NASA TO SUPPORT
+                <br /> SUSTAINED LUNAR
+                <br /> EXPLORATION
+              </h1>
+              <button className="content-button">LEARN MORE</button>
+            </div>
+          </div>
+        </section>
+        <section className="fifth-content">
+          <div className="content-size">
+            <div className="content-item">
+              <h1>STARSHIP UPDATE</h1>
+              <button className="content-button">LEARN MORE</button>
+            </div>
+          </div>
         </section>
       </main>
+
+      <footer>
+        <div className="footer"></div>
+      </footer>
     </div>
   )
 }
